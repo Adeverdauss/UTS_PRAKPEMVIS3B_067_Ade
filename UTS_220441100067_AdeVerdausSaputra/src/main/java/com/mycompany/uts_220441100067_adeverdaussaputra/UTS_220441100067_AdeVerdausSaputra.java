192bb6e3f2d6137910eff60b8c95688e929002ad
Ade Verdaus <60376000+Adeverdauss@users.noreply.github.com>
2023-10-18 18:06:39 +0700

    Add files via upload

---
+++ UTS_220441100067_AdeVerdausSaputra/src/main/java/com/mycompany/uts_220441100067_adeverdaussaputra/UTS_220441100067_AdeVerdausSaputra.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uts_220441100067_adeverdaussaputra;
+
+/**
+ *
+ * @author ASUS
+ */
+public class UTS_220441100067_AdeVerdausSaputra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
